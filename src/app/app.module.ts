35270b0354685218d0983b9ade2c49aaac4955f7
Malek Masri <malekmasri94@gmail.com>
2018-02-28 21:25:54 -0700

    Add routes for HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent} from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
