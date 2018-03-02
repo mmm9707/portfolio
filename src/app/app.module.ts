8530094c7b49658ecf168d8f8c8b4e66f7fee6d0
Malek Masri <malekmasri94@gmail.com>
2018-03-01 20:56:25 -0700

    Create resume page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent} from './home/home.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full'}
+      { path: 'resume', component: ResumeComponent,}
     ])
   ],
   providers: [],
