f80319b1d7594923cbecd863f465dc8327b0fa5a
Malek Masri <malekmasri94@gmail.com>
2018-03-01 21:31:56 -0700

    Updated resume page to be more visually appealing.

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'resume',
   templateUrl: './resume.component.html'
+  styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent {
   title = 'app';
