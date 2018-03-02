cc12558834e1392da1c6d0819f91f46191cacd2b
Malek Masri <malekmasri94@gmail.com>
2018-03-01 22:57:27 -0700

    Create footer and import angular-font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,6 +16,7 @@ import { ResumeComponent } from './resume/resume.component';
   ],
   imports: [
     BrowserModule,
+    AngularFontAwesomeModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full'},
       { path: 'resume', component: ResumeComponent,}
