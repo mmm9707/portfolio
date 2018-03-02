63c9162033e5094432d7483ce8cc9e4f8435752d
Malek Masri <malekmasri94@gmail.com>
2018-03-01 22:56:51 -0700

    Fixed a syntax error

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'resume',
-  templateUrl: './resume.component.html'
+  templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent {
